0a8b77f6f9e7364af07bcc727a545105bae48838
kylydigop <johnlloydlanon05@gmail.com>
2022-11-03 12:38:33 +0000

    test

--- test_code.py
+++ test_code.py
@@ -1,5 +1,9 @@
 from ipapi import get_location
 
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
 def get_ip(): 
     return ip(get_location())
 
