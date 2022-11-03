068ddd4b0666880bbdacb7a6b1b56839cff97b7f
kylydigop <johnlloydlanon05@gmail.com>
2022-11-03 11:34:18 +0000

    test

--- TESTING.py
+++ TESTING.py
@@ -1,3 +1,11 @@
 from Option2_Project import get_location
 
-print("hello,  world!")
+def get_length(): # Getting the Length of the Api
+    return len(get_location())
+
+def test_length(): # Testing the length on api
+    assert get_length() == 9
+
+def test_null(): # Check if values are not null
+    for i in get_location():
+        assert i != None
