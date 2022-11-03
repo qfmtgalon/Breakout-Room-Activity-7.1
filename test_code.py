b53767c880ef932eac5c1e9b6f96e20403b8ee9b
kylydigop <johnlloydlanon05@gmail.com>
2022-11-03 12:37:16 +0000

    test

--- test_code.py
+++ test_code.py
@@ -1,6 +1,7 @@
 from ipapi import get_location
 
-def get_postal(): 
-    return Postal(get_location())
-def test_postal():
-    assert get_postal() == 1811
+def get_ip(): 
+    return ip(get_location())
+
+def test_ip():
+    assert get_ip()
