8910a52e2472122d4e240a981546d0040010eff0
kylydigop <johnlloydlanon05@gmail.com>
2022-11-03 12:25:56 +0000

    test

--- test_code.py
+++ test_code.py
@@ -1,11 +1,4 @@
 from ipapi import get_location
 
-def get_length(): 
-    return len(get_location())
-
-def test_length(): 
-    assert get_length() == 9
-
-def test_null(): 
-    for i in get_location(): #test
-        assert i != None
+def get_postal(): 
+    return pos(get_location())
