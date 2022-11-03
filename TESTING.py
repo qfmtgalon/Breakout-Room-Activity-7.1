020a57c9c712b4aa1f5442859b20380bd927cf65
kylydigop <johnlloydlanon05@gmail.com>
2022-11-03 11:45:17 +0000

    test

--- TESTING.py
+++ TESTING.py
@@ -6,6 +6,6 @@ def get_length(): # Getting the Length of the Api
 def test_length(): # Testing the length on api
     assert get_length() == 9
 
-def test_null(): # Check if values are not null
+def test_null(): 
     for i in get_location():
         assert i != None
