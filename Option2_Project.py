9d902da662943121ed63cc32020943d3c13b561b
kylydigop <johnlloydlanon05@gmail.com>
2022-11-03 09:28:43 +0000

    New Features Update

--- Option2_Project.py
+++ Option2_Project.py
@@ -1,4 +1,6 @@
 import requests
+
+
 def get_ip():
     response = requests.get('https://api64.ipify.org?format=json').json()
     return response["ip"]
@@ -6,13 +8,15 @@ def get_location():
     ip_address = get_ip()
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
     location_data = {
-        "IP Address of the User": ip_address,
-        "Current Internet Protocol Version": response.get("version"),
-        "Current City": response.get("city"),
-        "Current Region": response.get("region"),
-        "Country": response.get("country_name"),
-        "Internet Service Provider": response.get("org"),
-        "Autonomous System Number": response.get("asn")
+        "IP Address of the User             ": ip_address,
+        "Current Internet Protocol Version  ": response.get("version"),
+        "Current City                       ": response.get("city"),
+        "Postal Code                        ": response.get("postal"),
+        "Current Region                     ": response.get("region"),
+        "User Country                       ": response.get("country_name"),
+        "Time Zone of the Country           ": response.get("timezone"),
+        "Internet Service Provider          ": response.get("org"),
+        "Autonomous System Number           ": response.get("asn")
     } 
    
     return location_data
